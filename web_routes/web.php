352d912ec146a7b4d20d70f5898dcbee716c930e
Jamshid <asilbek090900@gmail.com>
2025-02-23 10:00:41 -0800

    Routing Controller

---
+++ web_routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+//1---------------------
+Route::get('/city/{name}', function ($name) {
+    return "Welcome to {$name}!";
+})->name('city.show');
+
+//2------------------------------------------
+Route::prefix('admin')->group(function () {
+    Route::get('/users/{id}', function ($id) {
+        return "User ID: {$id}";
+    })->name('admin.users.show');
+});
+
+//3-----------------------------------------
+Route::prefix('profile')->group(function () {
+    Route::get('/view', function () {
+        return 'Profile View';
+    })->name('profile.view');
+    
+    Route::get('/edit', function () {
+        return 'Profile Edit';
+    })->name('profile.edit');
+    
+    Route::get('/settings', function () {
+        return 'Profile Settings';
+    })->name('profile.settings');
+});
+
+//4--------------------------------------------
+Route::prefix('products')->group(function () {
+    Route::get('/', function () {
+        return 'All Products';
+    });
+
+    Route::get('/{id}', function ($id) {
+        return "Product ID: {$id}";
+    });
+
+    Route::get('/{id}/edit', function ($id) {
+        return "Edit Product ID: {$id}";
+    })->name('products.edit');
+});
+//5--------------------------------
+Route::get('/new-url', function () {
+    return "Yangi URL";
+});
+
+Route::get('/old-url', function () {
+    return redirect('/new-url');
+});
